b68f58712d9d0146bd207b85e8bb68408f06ab5f
Arce <ruben.arce@perkinelmer.com>
2023-06-15 16:57:11 +0200

    add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { GameStatus, Puppet } from "./interfaces/puppet";
 import MainContainer from "./components/MainContainer";
 import data from './data/puppets.json';
+import { Button } from "@mui/material";
 
 function App() {
   const [people, setPeople] = useState<Puppet[]>([]);
+  const audioErrorRef = useRef();
+  const audioRandomRef = useRef();
 
   const setNextPuppet = () => {
     // Get the current speaker and move to sleeping
@@ -47,7 +50,7 @@ function App() {
       <div>
         <img src={"/images/doll.png"} className="logo react" />
       </div>
-      <div style={{ minHeight: "400px" }}>
+      <div style={{ minHeight: "300px" }}>
         <MainContainer listPuppets={people} />
       </div>
       <h1>{getNameToShow()}</h1>
@@ -58,6 +61,11 @@ function App() {
       <p className="read-the-docs">
         Tyranny and slavery is the only way
       </p>
+
+      <Button variant="contained"color="primary" onClick={() => audioErrorRef.current.play()}> Error </Button>
+      <audio style={{visibility: "hidden"}} src={"/audio/fail.mp3"} ref={audioErrorRef} controls />
+      <audio style={{visibility: "hidden"}} src={"/audio/random.mp3"} ref={audioRandomRef} controls />
+      <Button variant="contained"color="primary" onClick={() => audioRandomRef.current.play()}> Success </Button>
     </>
   );
 }
