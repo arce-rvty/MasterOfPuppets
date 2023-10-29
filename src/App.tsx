1428c0cf7649ea287c453106dee5fc0d1dc45f1d
RarceD <akimbo170@gmail.com>
2023-10-29 10:52:59 +0100

    Fix upper case service

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import MainImage from "./components/MainImage";
 import WaitingLoadFile from "./components/WaitingLoadFile";
 import NextPuppetButton from "./components/NextPuppetButton";
 import { PuppetOrder } from "./interfaces/order";
-import { OrderService } from "./services/orderService";
+import { OrderService } from "./services/OrderService";
 import { StatsService } from "./services/StatsService";
 
 function App() {
