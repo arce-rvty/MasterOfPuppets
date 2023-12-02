ff61192804f058a48cbca07f525dade2d513a9e3
RarceD <akimbo170@gmail.com>
2023-12-02 10:38:31 +0100

    Force icon in bundle

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function createWindow() {
         webPreferences: {
             // preload: path.join(__dirname, "preload.js")
         },
-        width: 1200
+        width: 1200,
+        icon : "./dist/images/doll.ico",
     });
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, "./dist/index.html"));
